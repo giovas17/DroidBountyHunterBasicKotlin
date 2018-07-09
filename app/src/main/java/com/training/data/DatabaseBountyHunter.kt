eb59b28b7048bb9e0de40429ae299e7e16567c83
Giovani Gonzalez <darkgeat@gmail.com>
2018-07-09 16:23:28 -0500

    Adding models and the initial database functionality

---
+++ app/src/main/java/com/training/data/DatabaseBountyHunter.kt
@@ -0,0 +1,113 @@
+package com.training.data
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.training.models.Fugitivo
+
+/**
+ * @author Giovani Gonzalez
+ * Created giovani on 7/9/18
+ */
+
+/** ------------------- Nombre de Base de Datos --------------------------**/
+const val DATABASE_NAME = "DroidBountyHunterDatabase"
+/** ------------------ Versión de Base de Datos --------------------------**/
+const val VERSION = 1
+/** ---------------------- Tablas y Campos -------------------------------**/
+const val TABLE_NAME_FUGITIVOS = "fugitivos"
+const val COLUMN_NAME_ID = "id"
+const val COLUMN_NAME_NAME = "name"
+const val COLUMN_NAME_STATUS = "status"
+
+class DatabaseBountyHunter(val context: Context){
+    private val TAG: String = DatabaseBountyHunter::class.java.simpleName
+    /** ------------------- Declaración de Tablas ----------------------------**/
+    private val TFugitivos = "CREATE TABLE " + TABLE_NAME_FUGITIVOS + " (" +
+            COLUMN_NAME_ID + " INTEGER PRIMARY KEY NOT NULL, " +
+            COLUMN_NAME_NAME + " TEXT NOT NULL, " +
+            COLUMN_NAME_STATUS + " INTEGER, " +
+            "UNIQUE (" + COLUMN_NAME_NAME + ") ON CONFLICT REPLACE);"
+    /** ---------------------- Variables y Helpers ---------------------------**/
+    private var helper: DBHelper? = null
+    private var database: SQLiteDatabase? = null
+
+    inner class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, VERSION) {
+        override fun onCreate(db: SQLiteDatabase?) {
+            Log.d(TAG, "Creación de la base de datos")
+            db!!.execSQL(TFugitivos)
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            Log.w(TAG, "Actualización de la BDD de la versión " + oldVersion + "a la " +
+                    newVersion + ", de la que se destruirá la información anterior")
+            // Destruir BDD anterior y crearla nuevamente las tablas actualizadas
+            db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_FUGITIVOS)
+            // Re-creando nuevamente la BDD actualizada
+            onCreate(db)
+        }
+
+    }
+
+    fun open(): DatabaseBountyHunter{
+        helper = DBHelper(context)
+        database = helper!!.writableDatabase
+        return this
+    }
+
+    fun close(){
+        helper!!.close()
+        database!!.close()
+    }
+
+    fun querySQL(sql: String, selectionArgs: Array<String>): Cursor{
+        open()
+        val regreso = database!!.rawQuery(sql,selectionArgs)
+        close()
+        return regreso
+    }
+
+    fun borrarFugitivo(fugitivo: Fugitivo){
+        open()
+        database!!.delete(TABLE_NAME_FUGITIVOS, COLUMN_NAME_ID + "=?", arrayOf(fugitivo.id.toString()))
+        close()
+    }
+
+    fun actualizarFugitivo(fugitivo: Fugitivo){
+        open()
+        val values = ContentValues()
+        values.put(COLUMN_NAME_NAME, fugitivo.name)
+        values.put(COLUMN_NAME_STATUS, fugitivo.status)
+        database!!.update(TABLE_NAME_FUGITIVOS,values, COLUMN_NAME_ID + "=?",arrayOf(fugitivo.id.toString()))
+        close()
+    }
+
+    fun insertarFugitivo(fugitivo: Fugitivo){
+        val values = ContentValues()
+        values.put(COLUMN_NAME_NAME, fugitivo.name)
+        values.put(COLUMN_NAME_STATUS, fugitivo.status)
+        open()
+        database!!.insert(TABLE_NAME_FUGITIVOS, null,values)
+        close()
+    }
+
+    fun obtenerFugitivos(status: Int) : Array<Fugitivo>? {
+        var fugitivos: Array<Fugitivo>? = null
+        val dataCursor = querySQL("SELECT * FROM " + TABLE_NAME_FUGITIVOS + " WHERE " +
+                COLUMN_NAME_STATUS + "= ? ORDER BY " + COLUMN_NAME_NAME, arrayOf(status.toString()))
+        if (dataCursor.count > 0) {
+            fugitivos = generateSequence {
+                if (dataCursor.moveToNext()) dataCursor else null
+            }.map {
+                val name = it.getString(it.getColumnIndex(COLUMN_NAME_NAME))
+                val statusFugitivo = it.getInt(it.getColumnIndex(COLUMN_NAME_STATUS))
+                val id = it.getInt(it.getColumnIndex(COLUMN_NAME_ID))
+                return@map Fugitivo(id,name,statusFugitivo)
+            }.toList().toTypedArray()
+        }
+        return fugitivos
+    }
+}
---
+++ app/src/main/java/com/training/models/Modelos.kt
@@ -0,0 +1,11 @@
+package com.training.models
+
+/**
+ * @author Giovani Gonzalez
+ * Created darkgeat on 7/9/18
+ */
+
+data class Fugitivo(
+        val id: Int = 0,
+        val name: String = "",
+        val status: Int = 0)
